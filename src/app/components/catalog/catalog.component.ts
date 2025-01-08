738c5ae122e8fa685adee52049004229832c58c0
JulioHerreraSe <julio.herreraseara@gmail.com>
2025-01-08 23:49:46 +0100

    Implementación de rutas

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,20 +1,30 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductCardComponent } from "../product-card/product-card.component";
+import { Router } from '@angular/router';
+import { SharingDataService } from '../../services/sharing-data.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'catalog',
   imports: [ProductCardComponent],
   templateUrl: './catalog.component.html',
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit{
 
-  @Input() products!: Product[];
+  products!: Product[];
 
-  @Output() productEventEmitter: EventEmitter<Product> = new EventEmitter();
+  constructor(
+    private productService: ProductService,
+    private sharingDataService: SharingDataService) {}
+  
+  ngOnInit(): void {
+      this.products = this.productService.findAll();
+  }
+;
 
   onAddCart(product: Product) {
-    this.productEventEmitter.emit(product);
+    this.sharingDataService.ProductEventEmitter.emit(product);
   }
 
 }
